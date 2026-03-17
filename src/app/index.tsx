f566c02619000b137152fede8ac22168afae7155
4arnab <axmedarnab3@gmail.com>
2026-03-17 07:25:13 +0530

    added nativewind for styling

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,10 +4,12 @@ import { StyleSheet, Text, View } from "react-native";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+      <Text className="text-lg text-green-500 underline">
+        Edit src/app/index.tsx to edit this screen.
+      </Text>
       <Image
         source={require("../../assets/images/react-logo.png")}
-        style={{ width: 200, height: 200 }}
+        className="w-12 h-12 mt-4 bg-blue-50 p-4 rounded-sm"
       />
     </View>
   );
